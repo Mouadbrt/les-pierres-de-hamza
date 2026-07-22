bb6cbee2296191a25093e81a3d4e9a3b3d858e35
MouaddV1 <mouaddbrt10@gmail.com>
2026-07-22 13:53:09 +0100

    Update website

--- src/data/businessInfo.js
+++ src/data/businessInfo.js
@@ -2,7 +2,7 @@ export const businessInfo = {
   companyName: 'Les Pierres de Hamza',
   phone: '+212 7 67 87 07 65',
   whatsapp: '212767870765',
-  email: 'Lespairresdehamza@gmail.com',
+  email: 'Lespierresdehamza@gmail.com',
   address: 'Taza, Maroc',
 }
 
